a139c41b6a6c9c0a7c9f7afd718a2be1cbe33440
aryanmungekar <145465879+aryanmungekar@users.noreply.github.com>
2025-06-26 19:56:38 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,12 @@
 // firebase-config.js
-// Firebase
+
+// ✅ Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyA1czRjhbfYGqGsGFul49vvjaSZnARSkVE",
   authDomain: "siteview-3bc59.firebaseapp.com",
   databaseURL: "https://siteview-3bc59-default-rtdb.firebaseio.com",
   projectId: "siteview-3bc59",
-  storageBucket: "siteview-3bc59.firebasestorage.app",
+  storageBucket: "siteview-3bc59.appspot.com", // ✅ corrected URL
   messagingSenderId: "941487179656",
   appId: "1:941487179656:web:6d7c3eb02b61a0856a9c3d",
   measurementId: "G-W9DN8D8HRM"
@@ -16,10 +17,9 @@ firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const db = firebase.database();
 
-// Cloudinary
+// ✅ Cloudinary
 const cloudinaryConfig = {
   cloudName: "siteview_thumbnails",
-  uploadPreset: "https://api.cloudinary.com/v1_1/${CLOUDINARY_CLOUD_NAME}/auto/upload"
-
-
+  uploadPreset: "your_unsigned_upload_preset", // set this correctly
+  apiUrl: "https://api.cloudinary.com/v1_1/siteview_thumbnails/auto/upload"
 };
